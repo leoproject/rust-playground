387587b300c842773f7f5617974e503bcf8760e6
leoproject <leonardo2013ufs@gmail.com>
2026-01-27 23:49:57 +0000

    Add initial project files for hello_cargo including Cargo.toml, Cargo.lock, and main.rs

---
+++ hello_cargo/target/debug/deps/hello_cargo-f14f03aed700b3e0.d
@@ -0,0 +1,5 @@
+/home/leonardo/Documents/rust-playground/hello_cargo/target/debug/deps/hello_cargo-f14f03aed700b3e0.d: src/main.rs
+
+/home/leonardo/Documents/rust-playground/hello_cargo/target/debug/deps/hello_cargo-f14f03aed700b3e0: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/leonardo/Documents/rust-playground/hello_cargo/target/debug/hello_cargo: /home/leonardo/Documents/rust-playground/hello_cargo/src/main.rs
