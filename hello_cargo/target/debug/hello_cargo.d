473009a51c1299d61971d2da4f1826c7a34a10f2
Leonardo Silva <leonardo.silva@ceiia.com>
2026-02-10 11:38:38 +0000

    finish chapter1

--- hello_cargo/target/debug/hello_cargo.d
+++ hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/home/leonardo/Documents/rust-playground/hello_cargo/target/debug/hello_cargo: /home/leonardo/Documents/rust-playground/hello_cargo/src/main.rs
+/home/leonardosilva/Documents/rust-playground/hello_cargo/target/debug/hello_cargo: /home/leonardosilva/Documents/rust-playground/hello_cargo/src/main.rs
