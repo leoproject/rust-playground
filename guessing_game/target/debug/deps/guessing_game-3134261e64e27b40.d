0fbe00a84292aa201e7701e1e225602c452ed762
leoproject <leonardo2013ufs@gmail.com>
2026-02-12 23:15:44 +0000

    Add guessing game

---
+++ guessing_game/target/debug/deps/guessing_game-3134261e64e27b40.d
@@ -0,0 +1,5 @@
+/home/leonardo/Documents/rust-playground/guessing_game/target/debug/deps/guessing_game-3134261e64e27b40.d: src/main.rs
+
+/home/leonardo/Documents/rust-playground/guessing_game/target/debug/deps/guessing_game-3134261e64e27b40: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/leonardo/Documents/rust-playground/guessing_game/target/debug/guessing_game: /home/leonardo/Documents/rust-playground/guessing_game/src/main.rs
